42054a0a90deba7982452620607d4455461b0a4d
James Garcia <james.garcia1201@outlook.com>
2022-07-21 11:01:23 -0500

    test

--- repository/TweetRepo.py
+++ repository/TweetRepo.py
@@ -11,6 +11,8 @@ nltk.download('wordnet')
 nltk.download('averaged_perceptron_tagger')
 nltk.download('maxent_ne_chunker')
 nltk.download('words')
+nltk.download('omw-1.4')
+
 
 def processTweets(tweets):
     response = {}

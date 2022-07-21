8a372453d8ff0bf39a7c4413ed43b97ec10be0f6
James Garcia <james.garcia1201@outlook.com>
2022-07-21 10:28:31 -0500

    added access tokens for deployment

--- service/TweetService.py
+++ service/TweetService.py
@@ -5,11 +5,18 @@ from dotenv import load_dotenv
 
 load_dotenv()
 token = os.environ.get("TOKEN")
-api = tweepy.Client(bearer_token=token, wait_on_rate_limit=True)
+consumer_key = os.environ.get("CONSUMER_KEY")
+consumer_secret = os.environ.get("CONSUMER_SECRET")
+access_token = os.environ.get("ACCESS_TOKEN")
+access_secret = os.environ.get("ACCESS_SECRET")
+
+api = tweepy.Client(bearer_token=token, consumer_key=consumer_key,
+                    consumer_secret=consumer_secret, access_token=access_token,
+                    access_token_secret=access_secret, wait_on_rate_limit=True)
 
 
 def getTweets(search_term):
-    tweets = api.search_recent_tweets(query=search_term, max_results=100)
+    tweets = api.search_recent_tweets(query=search_term, max_results=100, user_auth=True)
     tweet = tweets.data
     response = {}
     for i in range(len(tweet)):
